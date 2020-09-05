ed359ff18033282c0321ca7d7d640581ec22b805
happybirthday-dianne <70341503+happybirthday-dianne@users.noreply.github.com>
2020-09-05 09:59:02 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -114,7 +114,13 @@ const animationTimeline = () => {
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-4", 0.7, {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg",
+    delay:3
+  }, "+=1.5")
     .from(
       ".idea-5",
       0.7,
